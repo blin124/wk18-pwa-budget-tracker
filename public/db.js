c826424bfbcf425d1605565fc5414ec88f1bd820
Bao <lbx01072004@gmail.com>
2020-06-27 18:43:58 +1000

    final

---
+++ public/db.js
@@ -0,0 +1,30 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
